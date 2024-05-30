7270bae1e622133028fbf3bbdf36f5626d94747a
tylau0822 <34152633+tylau0822@users.noreply.github.com>
2024-05-30 18:20:59 -0400

    Initial commit

---
+++ schedule planner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  schedule planner
+//
+//  Created by LAU TSZ YING on 30/5/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
